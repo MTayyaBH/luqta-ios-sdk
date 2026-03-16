9cb157c7be4c510ad2566b6f55134e59ddfd401d
fazihamza <Faizan8887r@gmail.com>
2026-03-16 15:04:40 +0500

    Fix CFBundleExecutable in xcframework and update checksum

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "LuqtaSDK",
             url: "https://github.com/MTayyaBH/luqta-ios-sdk/releases/download/1.1.0/LuqtaSDK.xcframework.zip",
-            checksum: "a1ca49cdbee210c37c243826024a0a6ae1b2c0c949b1a1a9d38b4639685681a9"
+            checksum: "572b92219e22f24488e4254e55dcc0e74391fd060a9d37b682dbe1805d69988f"
         ),
     ]
 )
